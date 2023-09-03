aa7bf6e02e03cf12c525a7f2ac84f2be5d1e78f8
feuvpi <fredvpg@gmail.com>
2023-09-03 18:55:02 -0300

    initial commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function DashboardPage() {
+  return (
+    <>
+    <p className="text-6xl text-green-500">Hello SaaS AI</p>
+    <Button>Click Me!</Button>
+    
+    
+    </>
+    
+  )
+}
