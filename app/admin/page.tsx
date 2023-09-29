7d8b676432506c055ccbbb5c84f5e8549a01c72a
feuvpi <fredvpg@gmail.com>
2023-09-29 19:35:50 -0300

    init commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+export default function Admin() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl mb-8">Welcome!</h1>
+      <button className="p-2 w-40 border border-gray-400 rounded-md">
+        Sair
+      </button>
+    </div>
+  );
+}
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,46 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+
+
+const nextAuthOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password'}
+            },
+    
+            async authorize(credentials, req) {
+                const response = await fetch('http://localhost:8080/auth/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: credentials?.email,
+                        password: credentials?.password
+                    })
+                })
+
+                const user = await response.json()
+                if(response.ok && user){
+                    return user
+                }
+            },
+
+
+        })
+        
+
+    ],
+    pages: {
+        signIn: '/'
+    }
+}
+
+const handler = NextAuth(nextAuthOptions)
+
+export { handler as GET, handler as POST }
